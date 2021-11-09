a430f9edca9269d65ed7bc051c4545bbcb99d9de
GLeviosa <igigouz@gmail.com>
2021-11-08 23:32:48 -0300

    banco local/minor errors fixed

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from typing import Optional, List
 
 from fastapi import FastAPI, Path, Query, HTTPException
+from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel, Field
+import json
 
 app = FastAPI()
 
@@ -11,8 +13,18 @@ class Discipline(BaseModel):
     notes : List[str]
 
 dic = {}
+with open("db_local.json", "r") as file:
+    dic = json.load(file)
+
+
 success = {"status" : 200,
            "response" : {} }
+
+def att_db(dic):
+    dic_json = jsonable_encoder(dic)
+    dic_object = json.dumps(dic_json, indent=4)
+    with open("db_local.json", "w") as file:
+        file.write(dic_object)
 
 @app.get("/")
 def read_root():
@@ -20,44 +32,51 @@ def read_root():
 
 
 @app.get("/disciplines/{discipline_id}")
-def read_discipline(discipline_id: int):
+def read_discipline(discipline_id: str):
+    if discipline_id not in dic:
+        raise HTTPException(status_code=404, detail="discipline_id not found")
     success["response"] = dic[discipline_id]
     return success
 
 # Cria Disciplina
 @app.post("/disciplines/")
 async def create_discipline(discipline : Discipline):
-    if 0 not in dic:
-        dic[0] = discipline
+    if "0" not in dic:
+        dic["0"] = dict(discipline)
     else:
         is_in_dic = False
         for key, value in dic.items():
-            if discipline.name == value.name:
+            if discipline.name == value["name"]:
                 is_in_dic = True
         if is_in_dic:
             raise HTTPException(status_code=409, detail="Conflict, Already exists")
         else:    
-            new_index = list(dic)[-1] + 1
-            dic[new_index] = discipline
+            new_index = str(int(list(dic)[-1]) + 1)
+            dic[new_index] = dict(discipline)
 
     success["response"] = dic
+    att_db(dic)
     return success
 
 # Atualiza Disciplina
 @app.put("/disciplines/{discipline_id}")
-async def update_discipline(discipline : Discipline, discipline_id: int):
-    dic[discipline_id] = discipline
+async def update_discipline(discipline : Discipline, discipline_id: str):
+    if discipline_id not in dic:
+        raise HTTPException(status_code=404, detail="discipline_id not found")
+    dic[discipline_id] = dict(discipline)
+    att_db(dic)
     return dic
 
 # Deleta Disciplina
 @app.delete("/disciplines/{discipline_id}")
-async def delete_discipline(discipline_id : int):
+async def delete_discipline(discipline_id : str):
     deleted = dic.pop(discipline_id, False)
     if deleted:
         success["response"] = dic
+        att_db(dic)
         return success
     else:
-        raise HTTPException(status_code=404, detail="ID not found")
+        raise HTTPException(status_code=404, detail="discipline_id not found")
 
 # Lista Disciplinas
 @app.get("/disciplines/")
@@ -66,48 +85,56 @@ def show_all():
         "disciplines" : []
     }
     for value in dic.values():
-        names["disciplines"].append(value.name)
+        print(value)
+        print("-"*15)
+        names["disciplines"].append(value["name"])
     return names
 
-@app.patch("/notes/{discipline_id}")
-def add_note(discipline_id : int, note : str):
-    if discipline_id not in dic:
-        raise HTTPException(status_code=404, detail="ID not found")
-        
-    dic[discipline_id].notes.append(note)
-    success["response"] = dic
-    return success
-
+# Deleta disciplina
 @app.delete("/notes/{discipline_id}")
-def remove_note(discipline_id : int, note_index : int):
+def remove_note(discipline_id : str, note_index : int):
     if discipline_id not in dic:
         raise HTTPException(status_code=404, detail="Discipline_ID not found")
-    if note_index > len(dic[discipline_id].notes) -1:
-        raise HTTPException(status_code=404, detail="Note_ID not found")
-    dic[discipline_id].notes.pop(note_index)
+    if note_index > len(dic[discipline_id]["notes"]) -1:
+        raise HTTPException(status_code=404, detail="note_index doesn't exist")
+    dic[discipline_id]["notes"].pop(note_index)
     success["response"] = dic
+    att_db(dic)
     return success
 
+# Get notas
 @app.get("/notes/{discipline_id}")
-def show_notes(discipline_id : int):
+def show_notes(discipline_id : str):
     if discipline_id not in dic:
-        raise HTTPException(status_code=404, detail="ID not found")
+        raise HTTPException(status_code=404, detail="discipline_id not found")
     response = {
-        "notes" : dic[discipline_id].notes
+        "name"  : dic[discipline_id]["name"],
+        "notes" : dic[discipline_id]["notes"]
     }
-    return response
+    success["response"] = response
+    return success
 
-@app.patch("/notes/{discipline_id}")
-def mod_note(discipline_id : int, note_index : int, new_note : str):
+@app.post("/notes/{discipline_id}")
+def add_note(discipline_id : str, new_note : str):
     if discipline_id not in dic:
-        raise HTTPException(status_code=404, detail="Discipline_ID not found")
-    if note_index > len(dic[discipline_id].notes) -1:
-        raise HTTPException(status_code=404, detail="Note_ID not found")
-    dic[discipline_id].notes[note_index] = new_note
+        raise HTTPException(status_code=404, detail="discipline_id not found")
+    dic[discipline_id]["notes"].append(new_note)
+    
+    success["response"] = dic
+    att_db(dic)
+    return success
+
+@app.put("/notes/{discipline_id}")
+def mod_note(discipline_id : str, note_index : int, new_note : str):
+    print(new_note)
+    print("-"*15)
+    if discipline_id not in dic:
+        raise HTTPException(status_code=404, detail="discipline_id not found")
+    if note_index > len(dic[discipline_id]["notes"]) -1:
+        raise HTTPException(status_code=404, detail="note_index doesn't exist")
+    dic[discipline_id]["notes"][note_index] = new_note
 
     success["response"] = dic
+    att_db(dic)
     return success
     
-# @app.put("/items/{item_id}")
-# async def create_item(item_id: int, item: Item):
-#     return {"item_id": item_id, **item.dict()}
